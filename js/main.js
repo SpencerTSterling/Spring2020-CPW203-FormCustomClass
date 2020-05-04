10d2728bd0d8c483d75f79f542bb11eb84923293
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-05-04 15:06:27 -0700

    Made getVideoGame() method functional

--- js/main.js
+++ js/main.js
@@ -18,6 +18,18 @@ function isAllDataValid() {
 }
 function displayGame(myGame) {
 }
+function getById(id) {
+    return document.getElementById(id);
+}
 function getVideoGame() {
-    return VideoGame;
+    var game = new VideoGame();
+    var titleInput = getById("title");
+    var priceInput = getById("price");
+    var ratingInput = getById("rating");
+    var isDigital = getById("online");
+    game.title = titleInput.value;
+    game.price = parseFloat(priceInput.value);
+    game.rating = ratingInput.value;
+    game.isOnlineOnly = isDigital.checked;
+    return game;
 }
--- ts/main.ts
+++ ts/main.ts
@@ -37,9 +37,26 @@ function displayGame(myGame:VideoGame):void{
     //Display Video Game below the form
 }
 
+
+function getById(id:string){
+    return document.getElementById(id);
+}
+
 function getVideoGame():VideoGame{
     // create game
+    let game = new VideoGame();
     // populate with data from the form
+    let titleInput = <HTMLInputElement>getById("title");
+    let priceInput = <HTMLInputElement>getById("price");
+    let ratingInput = <HTMLInputElement>getById("rating");
+    let isDigital = <HTMLInputElement>getById("online");
+
+
+    game.title = titleInput.value;
+    game.price = parseFloat(priceInput.value);
+    game.rating = ratingInput.value;
+    game.isOnlineOnly = isDigital.checked;
+
     // return game
-    return VideoGame;
+    return game;
 }
